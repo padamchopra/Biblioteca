037c46212e048a0c17f788aff153fd5ffc09737f
padamchopra <jb.padamchopra@gmail.com>
2021-10-27 18:26:22 -0400

    v0.1 for testing library publish

--- app/src/main/java/me/padamchopra/biblioteca/MainActivity.kt
+++ app/src/main/java/me/padamchopra/biblioteca/MainActivity.kt
@@ -8,6 +8,7 @@ import androidx.compose.material.Surface
 import androidx.compose.material.Text
 import androidx.compose.runtime.Composable
 import androidx.compose.ui.tooling.preview.Preview
+import me.padamchopra.biblioteca.navigation.BibliotecaApp
 import me.padamchopra.biblioteca.ui.theme.BibliotecaTheme
 
 class MainActivity : ComponentActivity() {
@@ -16,23 +17,8 @@ class MainActivity : ComponentActivity() {
         setContent {
             BibliotecaTheme {
                 // A surface container using the 'background' color from the theme
-                Surface(color = MaterialTheme.colors.background) {
-                    Greeting("Android")
-                }
+                BibliotecaApp()
             }
         }
-    }
-}
-
-@Composable
-fun Greeting(name: String) {
-    Text(text = "Hello $name!")
-}
-
-@Preview(showBackground = true)
-@Composable
-fun DefaultPreview() {
-    BibliotecaTheme {
-        Greeting("Android")
     }
 }
--- app/src/main/java/me/padamchopra/biblioteca/navigation/BibliotecaApp.kt
+++ app/src/main/java/me/padamchopra/biblioteca/navigation/BibliotecaApp.kt
@@ -1,9 +1,23 @@
 package me.padamchopra.biblioteca.navigation
 
+import androidx.compose.foundation.layout.Arrangement
+import androidx.compose.foundation.layout.Column
+import androidx.compose.foundation.layout.fillMaxSize
+import androidx.compose.material.Text
 import androidx.compose.runtime.Composable
-import androidx.navigation.compose.rememberNavController
+import androidx.compose.ui.Alignment
+import androidx.compose.ui.Modifier
+import androidx.compose.ui.res.stringResource
+import me.padamchopra.biblioteca.R
 
 @Composable
 fun BibliotecaApp() {
-    val navController = rememberNavController()
+    //val navController = rememberNavController()
+    Column(
+        modifier = Modifier.fillMaxSize(),
+        verticalArrangement = Arrangement.Center,
+        horizontalAlignment = Alignment.CenterHorizontally
+    ) {
+        Text(stringResource(id = R.string.app_name))
+    }
 }
