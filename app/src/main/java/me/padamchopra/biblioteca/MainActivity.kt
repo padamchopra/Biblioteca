39e7dc64124bbabd586131848a4c060eb288dd5b
padamchopra <jb.padamchopra@gmail.com>
2021-10-27 19:40:26 -0400

    v0.3

--- app/src/main/java/me/padamchopra/biblioteca/MainActivity.kt
+++ app/src/main/java/me/padamchopra/biblioteca/MainActivity.kt
@@ -3,11 +3,6 @@ package me.padamchopra.biblioteca
 import android.os.Bundle
 import androidx.activity.ComponentActivity
 import androidx.activity.compose.setContent
-import androidx.compose.material.MaterialTheme
-import androidx.compose.material.Surface
-import androidx.compose.material.Text
-import androidx.compose.runtime.Composable
-import androidx.compose.ui.tooling.preview.Preview
 import me.padamchopra.biblioteca.navigation.BibliotecaApp
 import me.padamchopra.biblioteca.ui.theme.BibliotecaTheme
 
