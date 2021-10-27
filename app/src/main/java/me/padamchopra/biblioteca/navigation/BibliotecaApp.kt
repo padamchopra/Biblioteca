5d0512a56d906e294faeaa93b95c9b2d7af83fe7
padamchopra <jb.padamchopra@gmail.com>
2021-10-26 23:10:29 -0400

    Initial commit

---
+++ app/src/main/java/me/padamchopra/biblioteca/navigation/BibliotecaApp.kt
@@ -0,0 +1,9 @@
+package me.padamchopra.biblioteca.navigation
+
+import androidx.compose.runtime.Composable
+import androidx.navigation.compose.rememberNavController
+
+@Composable
+fun BibliotecaApp() {
+    val navController = rememberNavController()
+}
